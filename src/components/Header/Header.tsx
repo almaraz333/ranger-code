69cd518e964aafc3d6beab853edb9c11a56ed4b7
Colton Almaraz <almarazcolton333@gmail.com>
2022-06-07 22:00:35 -0700

    Feature: added mobile menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
 import { HeaderProps } from "./types";
 import "./Header.scss";
+import { useState } from "react";
 
 export const Header: React.FC<HeaderProps> = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuOpen = () => {
+    setIsMobileMenuOpen(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const handleMobileMenuClose = () => {
+    setIsMobileMenuOpen(false);
+    document.body.style.overflow = "visible";
+  };
   return (
     <header className="header">
       <div>ranger code</div>
+      <FontAwesomeIcon onClick={handleMobileMenuOpen} size="2x" className="mobile-menu-icon" icon={faBars} />
       <div className="header-links">
         <a href="#whoWeAre">Who We Are</a>
         <a href="#whatWeDo">What We Do</a>
@@ -13,6 +29,30 @@ export const Header: React.FC<HeaderProps> = () => {
           <span>Contact</span>
         </a>
       </div>
+      <nav className={`mobile-nav-bar ${isMobileMenuOpen && "mobile-nav-bar-open"}`}>
+        <FontAwesomeIcon
+          onClick={handleMobileMenuClose}
+          size="2x"
+          className="mobile-menu-close-icon"
+          icon={faXmark}
+          color="#fff"
+        />
+
+        <div className="mobile-header-links">
+          <a onClick={handleMobileMenuClose} href="#whoWeAre">
+            Who We Are
+          </a>
+          <a onClick={handleMobileMenuClose} href="#whatWeDo">
+            What We Do
+          </a>
+          <a onClick={handleMobileMenuClose} href="#philosophy">
+            Our Philosophy
+          </a>
+          <a onClick={handleMobileMenuClose} href="#contact">
+            <span>Contact</span>
+          </a>
+        </div>
+      </nav>
     </header>
   );
 };
