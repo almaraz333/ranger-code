a28b1a49995f71fac0119bd3914db7bbebcccf50
Colton Almaraz <almarazcolton333@gmail.com>
2022-06-08 20:03:10 -0700

    Fix: better header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,20 +7,22 @@ import { useState } from "react";
 
 export const Header: React.FC<HeaderProps> = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleMobileMenuOpen = () => {
     setIsMobileMenuOpen(true);
-    document.body.style.overflow = "hidden";
+    // document.body.style.overflow = "hidden";
   };
 
   const handleMobileMenuClose = () => {
     setIsMobileMenuOpen(false);
-    document.body.style.overflow = "visible";
+    // document.body.style.overflow = "visible";
   };
   return (
     <header className="header">
       <div>ranger code</div>
-      <FontAwesomeIcon onClick={handleMobileMenuOpen} size="2x" className="mobile-menu-icon" icon={faBars} />
+
+      {/* <FontAwesomeIcon onClick={handleMobileMenuOpen} size="2x" className="mobile-menu-icon" icon={faBars} /> */}
       <div className="header-links">
         <a href="#whoWeAre">Who We Are</a>
         <a href="#whatWeDo">What We Do</a>
@@ -30,13 +32,13 @@ export const Header: React.FC<HeaderProps> = () => {
         </a>
       </div>
       <nav className={`mobile-nav-bar ${isMobileMenuOpen && "mobile-nav-bar-open"}`}>
-        <FontAwesomeIcon
+        {/* <FontAwesomeIcon
           onClick={handleMobileMenuClose}
           size="2x"
           className="mobile-menu-close-icon"
           icon={faXmark}
           color="#fff"
-        />
+        /> */}
 
         <div className="mobile-header-links">
           <a onClick={handleMobileMenuClose} href="#whoWeAre">
@@ -53,6 +55,20 @@ export const Header: React.FC<HeaderProps> = () => {
           </a>
         </div>
       </nav>
+      <div
+        className={`mobile-nav-button ${isMobileMenuOpen && "mobile-nav-button-open"}`}
+        data-mdb-toggle="collapse"
+        aria-controls="navbarToggleExternalContent9"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+      >
+        <div className={`animated-icon1 ${isMobileMenuOpen && "open"}`}>
+          <span className={`${isMobileMenuOpen && "open"}`}></span>
+          <span className={`${isMobileMenuOpen && "open"}`}></span>
+          <span className={`${isMobileMenuOpen && "open"}`}></span>
+        </div>
+      </div>
     </header>
   );
 };
